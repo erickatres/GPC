fceb1292969fd52653b1b22bbed088b435dffe08
Ericka Brudo <erickabrudo2@gmail.com>
2025-10-29 17:53:55 +0800

    Initial commit to GPC repo

--- app/employment/forgot_password/page.tsx
+++
@@ -1,9 +0,0 @@
-import { ForgotPasswordForm } from "@/app/components/forgot_password"
-
-export default function ForgotPasswordPage() {
-  return (
-    <div className="min-h-screen bg-slate-100 flex items-center justify-center p-4">
-      <ForgotPasswordForm />
-    </div>
-  )
-}
